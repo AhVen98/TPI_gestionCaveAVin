8bf7102ce86fa7c0d1f3c29bca8a0ef46fe7542d
Paola Costa <paola.costa98@gmail.com>
2021-05-11 16:55:35 +0200

    création de script relatifs à différentes tables pour l'insertion de données
    une fois tous les scripts complets et fonctionnels, ils seront fusionnés en 1 seul

---
+++ docs_avec_modification/baseDeDonnees/donnees generees/casiers.sql
@@ -0,0 +1,34 @@
+-- -----------------------------------------------------
+-- utilisation du schema wineManager
+-- -----------------------------------------------------
+USE `wineManager` ;
+
+-- -----------------------------------------------------
+-- insertion des données dans la table 'storageBoxes'
+-- -----------------------------------------------------
+
+INSERT INTO storageBoxes (name, location) VALUES ('A1','ligne a casier 1');
+INSERT INTO storageBoxes (name, location) VALUES ('A2','ligne a casier 2');
+INSERT INTO storageBoxes (name, location) VALUES ('A3','ligne a casier 3');
+INSERT INTO storageBoxes (name, location) VALUES ('A4','ligne a casier 4');
+INSERT INTO storageBoxes (name, location) VALUES ('A5','ligne a casier 5');
+INSERT INTO storageBoxes (name, location) VALUES ('B1','ligne b casier 1');
+INSERT INTO storageBoxes (name, location) VALUES ('B2','ligne b casier 2');
+INSERT INTO storageBoxes (name, location) VALUES ('B3','ligne b casier 3');
+INSERT INTO storageBoxes (name, location) VALUES ('B4','ligne b casier 4');
+INSERT INTO storageBoxes (name, location) VALUES ('B5','ligne b casier 5');
+INSERT INTO storageBoxes (name, location) VALUES ('C1','ligne c casier 1');
+INSERT INTO storageBoxes (name, location) VALUES ('C2','ligne c casier 2');
+INSERT INTO storageBoxes (name, location) VALUES ('C3','ligne c casier 3');
+INSERT INTO storageBoxes (name, location) VALUES ('C4','ligne c casier 4');
+INSERT INTO storageBoxes (name, location) VALUES ('C5','ligne c casier 5');
+INSERT INTO storageBoxes (name, location) VALUES ('D1','ligne d casier 1');
+INSERT INTO storageBoxes (name, location) VALUES ('D2','ligne d casier 2');
+INSERT INTO storageBoxes (name, location) VALUES ('D3','ligne d casier 3');
+INSERT INTO storageBoxes (name, location) VALUES ('D4','ligne d casier 4');
+INSERT INTO storageBoxes (name, location) VALUES ('D5','ligne d casier 5');
+INSERT INTO storageBoxes (name, location) VALUES ('E1','ligne e casier 1');
+INSERT INTO storageBoxes (name, location) VALUES ('E2','ligne e casier 2');
+INSERT INTO storageBoxes (name, location) VALUES ('E3','ligne e casier 3');
+INSERT INTO storageBoxes (name, location) VALUES ('E4','ligne e casier 4');
+INSERT INTO storageBoxes (name, location) VALUES ('E5','ligne e casier 5');
---
+++ docs_avec_modification/baseDeDonnees/donnees generees/robe.sql
@@ -0,0 +1,11 @@
+-- -----------------------------------------------------
+-- utilisation du schema wineManager
+-- -----------------------------------------------------
+USE `wineManager` ;
+
+-- -----------------------------------------------------
+-- insertion des données dans la table 'colors'
+-- -----------------------------------------------------
+INSERT INTO `colors` (wineColor) VALUES ('rouge');
+INSERT INTO `colors` (wineColor) VALUES ('rose');
+INSERT INTO `colors` (wineColor) VALUES ('blanc');
